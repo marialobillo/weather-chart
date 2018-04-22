3c43a2b64d9f31fa6ac1fd0caee943812d23ac12
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-22 20:57:53 +0200

    ajax request with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios form 'axios';
+
+
 const API_KEY = '1950864f7ff27d459cc2c7c3462b0430';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
