719a0eda65940dc58c468ba60c8f472fa0cee7bf
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-23 12:27:31 +0200

    fixing bugs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
-import axios form 'axios';
-
+import axios from 'axios';
 
 const API_KEY = '1950864f7ff27d459cc2c7c3462b0430';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
@@ -9,6 +8,8 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city){
 	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get(url);
+
+	console.log('Request: ', request);
 
 	return {
 		type: FETCH_WEATHER,
