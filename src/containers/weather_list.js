42cb1090940f38904d6461ac62cb74e0cea1900f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-23 20:07:31 +0200

    sparklines and charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,27 +1,53 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Chart from '../components/chart';
 
-class WeatherList extends Component{
-	render(){
+class WeatherList extends Component {
+	renderWeather(cityData) {
+		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const { lon, lat } = cityData.city.coord;
+
+		return (
+			<tr key={name}>
+				<td>{name}</td>
+				<td>
+					<Chart data={temps} color="orange" />
+				</td>
+				<td>
+					<Chart data={pressures} color="blue" />
+				</td>
+				<td>
+					<Chart data={humidities} color="green" />
+				</td>
+
+			</tr>
+		);
+	}
+
+	render() {
 		return (
 			<table className="table table-hover">
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
+					{this.props.weather.map(this.renderWeather)}
 				</tbody>
 			</table>
 		);
 	}
 }
 
-function mapStateToProps({weather}){
-	return { weather};
+function mapStateToProps({ weather }) {
+	return { weather };
 }
 
 export default connect(mapStateToProps)(WeatherList);
